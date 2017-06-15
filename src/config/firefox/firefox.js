6d7c3c23a0682ae4c4024f8ebf7323c14888dc77
inu1255 <929909260@qq.com>
2017-06-15 17:11:54 +0800

    码云git

--- src/config/firefox/firefox.js
+++ src/config/firefox/firefox.js
@@ -2,16 +2,16 @@ const data = require('sdk/self').data
 const pageMod = require('sdk/page-mod')
 
 pageMod.PageMod({
-  include: ['https://bitbucket.org/*', 'https://github.com/*'],
-  contentScriptFile : [data.url('jquery.js'),
-                       data.url('jquery-ui.js'),
-                       data.url('jstree.js'),
-                       data.url('keymaster.js'),
-                       data.url('ondemand.js'),
-                       data.url('octotree.js')
-                      ],
-  contentStyleFile  : [data.url('jstree.css'),
-                       data.url('octotree.css')
-                      ],
-  contentScriptWhen : 'start'
+    include: ['https://bitbucket.org/*', 'https://github.com/*', 'http://git.oschina.net/*'],
+    contentScriptFile: [data.url('jquery.js'),
+        data.url('jquery-ui.js'),
+        data.url('jstree.js'),
+        data.url('keymaster.js'),
+        data.url('ondemand.js'),
+        data.url('octotree.js')
+    ],
+    contentStyleFile: [data.url('jstree.css'),
+        data.url('octotree.css')
+    ],
+    contentScriptWhen: 'start'
 })
