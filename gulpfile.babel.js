c855e8fdb195e143bfa0d0c52c3a8ceb1cd9b98b
inu1255 <929909260@qq.com>
2017-06-15 16:05:27 +0800

    码云git

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,208 +1,209 @@
 import gulp from 'gulp'
 import fs from 'fs'
 import path from 'path'
-import {merge} from 'event-stream'
+import { merge } from 'event-stream'
 import map from 'map-stream'
-import {spawn} from 'child_process'
+import { spawn } from 'child_process'
 const $ = require('gulp-load-plugins')()
 const version = require('./package.json').version
 
 // Tasks
 gulp.task('clean', () => {
-  return pipe('./tmp', $.clean())
+    return pipe('./tmp', $.clean())
 })
 
 gulp.task('build', (cb) => {
-  $.runSequence('clean', 'styles', 'chrome', 'opera', 'safari', 'firefox', cb)
+    $.runSequence('clean', 'styles', 'chrome', 'opera', 'safari', 'firefox', cb)
 })
 
 gulp.task('default', ['build'], () => {
-  gulp.watch(['./libs/**/*', './src/**/*'], ['default'])
+    gulp.watch(['./libs/**/*', './src/**/*'], ['default'])
 })
 
 gulp.task('dist', ['build'], (cb) => {
-  $.runSequence('firefox:xpi', 'chrome:zip', 'chrome:crx', 'opera:nex', cb)
+    $.runSequence('firefox:xpi', 'chrome:zip', 'chrome:crx', 'opera:nex', cb)
 })
 
 gulp.task('test', ['build'], (cb) => {
-  const ps = spawn(
-    './node_modules/.bin/mocha',
-    ['--harmony', '--reporter', 'spec', '--bail', '--recursive', '--timeout', '-1']
-  )
-  ps.stdout.pipe(process.stdout);
-  ps.stderr.pipe(process.stderr);
-  ps.on('close', cb)
+    const ps = spawn(
+        './node_modules/.bin/mocha',
+        ['--harmony', '--reporter', 'spec', '--bail', '--recursive', '--timeout', '-1']
+    )
+    ps.stdout.pipe(process.stdout);
+    ps.stderr.pipe(process.stderr);
+    ps.on('close', cb)
 })
 
 gulp.task('styles', () => {
-  return pipe(
-    './src/styles/octotree.less',
-    $.plumber(),
-    $.less({relativeUrls: true}),
-    $.autoprefixer({cascade: true}),
-    './tmp'
-  )
+    return pipe(
+        './src/styles/octotree.less',
+        $.plumber(),
+        $.less({ relativeUrls: true }),
+        $.autoprefixer({ cascade: true }),
+        './tmp'
+    )
 })
 
 gulp.task('lib:ondemand', (cb) => {
-  const dir = './libs/ondemand'
-  const code = fs.readdirSync(dir).map(file => {
-    return `window['${file}'] = function () {
+    const dir = './libs/ondemand'
+    const code = fs.readdirSync(dir).map(file => {
+        return `window['${file}'] = function () {
       ${fs.readFileSync(path.join(dir, file))}
     };\n`
-  }).join('')
+    }).join('')
 
-  fs.writeFileSync('./tmp/ondemand.js', code)
+    fs.writeFileSync('./tmp/ondemand.js', code)
 
-  cb()
+    cb()
 })
 
 // Chrome
 gulp.task('chrome:template', () => {
-  return buildTemplate({CHROME: true})
+    return buildTemplate({ CHROME: true })
 })
 
 gulp.task('chrome:js', ['chrome:template', 'lib:ondemand'], () => {
-  return buildJs(['./src/config/chrome/overrides.js'], {CHROME: true})
+    return buildJs(['./src/config/chrome/overrides.js'], { CHROME: true })
 })
 
 gulp.task('chrome', ['chrome:js'], () => {
-  return merge(
-    pipe('./icons/**/*', './tmp/chrome/icons'),
-    pipe('./fonts/**/*', './tmp/chrome/fonts'),
-    pipe(['./libs/**/*', '!./libs/ondemand{,/**}', './tmp/octotree.*', './tmp/ondemand.js'], './tmp/chrome/'),
-    pipe('./src/config/chrome/background.js', $.babel(), './tmp/chrome/'),
-    pipe('./src/config/chrome/manifest.json', $.replace('$VERSION', version), './tmp/chrome/')
-  )
+    return merge(
+        pipe('./icons/**/*', './tmp/chrome/icons'),
+        pipe('./fonts/**/*', './tmp/chrome/fonts'),
+        pipe(['./libs/**/*', '!./libs/ondemand{,/**}', './tmp/octotree.*', './tmp/ondemand.js'], './tmp/chrome/'),
+        pipe('./src/config/chrome/background.js', $.babel(), './tmp/chrome/'),
+        pipe('./src/config/chrome/manifest.json', $.replace('$VERSION', version), './tmp/chrome/')
+    )
 })
 
 gulp.task('chrome:zip', () => {
-  return pipe('./tmp/chrome/**/*', $.zip('chrome.zip'), './dist')
+    return pipe('./tmp/chrome/**/*', $.zip('chrome.zip'), './dist')
 })
 
 gulp.task('chrome:_crx', (cb) => {
-  $.run('"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"' +
-    ' --pack-extension=' + path.join(__dirname, './tmp/chrome') +
-    ' --pack-extension-key=' + path.join(process.env.HOME, '.ssh/chrome.pem')
-  ).exec(cb)
+    $.run('"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"' +
+        ' --pack-extension=' + path.join(__dirname, './tmp/chrome') +
+        ' --pack-extension-key=' + path.join(process.env.HOME, '.ssh/chrome.pem')
+    ).exec(cb)
 })
 
 gulp.task('chrome:crx', ['chrome:_crx'], () => {
-  return pipe('./tmp/chrome.crx', './dist')
+    return pipe('./tmp/chrome.crx', './dist')
 })
 
 // Opera
 gulp.task('opera', ['chrome'], () => {
-  return pipe('./tmp/chrome/**/*', './tmp/opera')
+    return pipe('./tmp/chrome/**/*', './tmp/opera')
 })
 
 gulp.task('opera:nex', () => {
-  return pipe('./dist/chrome.crx', $.rename('opera.nex'), './dist')
+    return pipe('./dist/chrome.crx', $.rename('opera.nex'), './dist')
 })
 
 // Firefox
 gulp.task('firefox:template', () => {
-  return buildTemplate({FIREFOX: true})
+    return buildTemplate({ FIREFOX: true })
 })
 
 gulp.task('firefox:js', ['firefox:template', 'lib:ondemand'], () => {
-  return buildJs([], {FIREFOX: true})
+    return buildJs([], { FIREFOX: true })
 })
 
 gulp.task('firefox', ['firefox:js'], () => {
-  return merge(
-    pipe('./icons/**/*', './tmp/firefox/data/icons'),
-    pipe(['./libs/**/*', '!./libs/ondemand{,/**}', './tmp/octotree.*', './tmp/ondemand.js'], './tmp/firefox/data'),
-    pipe('./src/config/firefox/firefox.js', $.babel(), './tmp/firefox/lib'),
-    pipe('./src/config/firefox/package.json', $.replace('$VERSION', version), './tmp/firefox')
-  )
+    return merge(
+        pipe('./icons/**/*', './tmp/firefox/data/icons'),
+        pipe(['./libs/**/*', '!./libs/ondemand{,/**}', './tmp/octotree.*', './tmp/ondemand.js'], './tmp/firefox/data'),
+        pipe('./src/config/firefox/firefox.js', $.babel(), './tmp/firefox/lib'),
+        pipe('./src/config/firefox/package.json', $.replace('$VERSION', version), './tmp/firefox')
+    )
 })
 
 gulp.task('firefox:xpi', (cb) => {
-  $.run('cd ./tmp/firefox && ../../node_modules/.bin/jpm xpi && mkdir -p ../../dist && mv jid1-Om7eJGwA1U8Akg*.xpi ../../dist/firefox.xpi').exec(cb)
+    $.run('cd ./tmp/firefox && ../../node_modules/.bin/jpm xpi && mkdir -p ../../dist && mv jid1-Om7eJGwA1U8Akg*.xpi ../../dist/firefox.xpi').exec(cb)
 })
 
 // Safari
 gulp.task('safari:template', () => {
-  return buildTemplate({SAFARI: true})
+    return buildTemplate({ SAFARI: true })
 })
 
 gulp.task('safari:js', ['safari:template', 'lib:ondemand'], () => {
-  return buildJs([], {SAFARI: true})
+    return buildJs([], { SAFARI: true })
 })
 
 gulp.task('safari', ['safari:js'], () => {
-  return merge(
-    pipe('./icons/icon64.png', $.rename('Icon-64.png'), './tmp/safari/octotree.safariextension'),
-    pipe(
-      ['./libs/**/*', '!./libs/ondemand{,/**}', './tmp/octotree.*', './tmp/ondemand.js'],
-      './tmp/safari/octotree.safariextension/'
-    ),
-    pipe('./src/config/safari/Info.plist', $.replace('$VERSION', version), './tmp/safari/octotree.safariextension')
-  )
+    return merge(
+        pipe('./icons/icon64.png', $.rename('Icon-64.png'), './tmp/safari/octotree.safariextension'),
+        pipe(
+            ['./libs/**/*', '!./libs/ondemand{,/**}', './tmp/octotree.*', './tmp/ondemand.js'],
+            './tmp/safari/octotree.safariextension/'
+        ),
+        pipe('./src/config/safari/Info.plist', $.replace('$VERSION', version), './tmp/safari/octotree.safariextension')
+    )
 })
 
 // Helpers
 function pipe(src, ...transforms) {
-  return transforms.reduce((stream, transform) => {
-    const isDest = typeof transform === 'string'
-    return stream.pipe(isDest ? gulp.dest(transform) : transform)
-  }, gulp.src(src))
+    return transforms.reduce((stream, transform) => {
+        const isDest = typeof transform === 'string'
+        return stream.pipe(isDest ? gulp.dest(transform) : transform)
+    }, gulp.src(src))
 }
 
 function html2js(template) {
-  return map(escape)
+    return map(escape)
 
-  function escape(file, cb) {
-    const path = $.util.replaceExtension(file.path, '.js')
-    const content = file.contents.toString()
-    const escaped = content
-      .replace(/\\/g, "\\\\")
-      .replace(/'/g, "\\'")
-      .replace(/\r?\n/g, "\\n' +\n    '")
-    const body = template.replace('$$', escaped)
+    function escape(file, cb) {
+        const path = $.util.replaceExtension(file.path, '.js')
+        const content = file.contents.toString()
+        const escaped = content
+            .replace(/\\/g, "\\\\")
+            .replace(/'/g, "\\'")
+            .replace(/\r?\n/g, "\\n' +\n    '")
+        const body = template.replace('$$', escaped)
 
-    file.path = path
-    file.contents = new Buffer(body)
-    cb(null, file)
-  }
+        file.path = path
+        file.contents = new Buffer(body)
+        cb(null, file)
+    }
 }
 
 function buildJs(overrides, ctx) {
-  const src = [
-    './tmp/template.js',
-    './src/constants.js',
-    './src/adapters/adapter.js',
-    './src/adapters/bitbucket.js',
-    './src/adapters/github.js',
-    './src/view.help.js',
-    './src/view.error.js',
-    './src/view.tree.js',
-    './src/view.options.js',
-    './src/util.location.js',
-    './src/util.module.js',
-    './src/util.async.js',
-    './src/util.storage.js'
-  ].concat(overrides)
-   .concat('./src/octotree.js')
+    const src = [
+        './tmp/template.js',
+        './src/constants.js',
+        './src/adapters/adapter.js',
+        './src/adapters/bitbucket.js',
+        './src/adapters/github.js',
+        './src/adapters/oschina.js',
+        './src/view.help.js',
+        './src/view.error.js',
+        './src/view.tree.js',
+        './src/view.options.js',
+        './src/util.location.js',
+        './src/util.module.js',
+        './src/util.async.js',
+        './src/util.storage.js'
+    ].concat(overrides)
+        .concat('./src/octotree.js')
 
-  return pipe(
-    src,
-    $.babel(),
-    $.concat('octotree.js'),
-    $.preprocess({context: ctx}),
-    './tmp'
-  )
+    return pipe(
+        src,
+        $.babel(),
+        $.concat('octotree.js'),
+        $.preprocess({ context: ctx }),
+        './tmp'
+    )
 }
 
 function buildTemplate(ctx) {
-  const LOTS_OF_SPACES = new Array(500).join(' ')
+    const LOTS_OF_SPACES = new Array(500).join(' ')
 
-  return pipe(
-    './src/template.html',
-    $.preprocess({context: ctx}),
-    $.replace('__SPACES__', LOTS_OF_SPACES),
-    html2js('const TEMPLATE = \'$$\''),
-    './tmp'
-  )
+    return pipe(
+        './src/template.html',
+        $.preprocess({ context: ctx }),
+        $.replace('__SPACES__', LOTS_OF_SPACES),
+        html2js('const TEMPLATE = \'$$\''),
+        './tmp'
+    )
 }
---
+++ src/adapters/oschina.js
@@ -0,0 +1,187 @@
+const OSC_RESERVED_USER_NAMES = [
+    'settings', 'orgs', 'organizations',
+    'site', 'blog', 'about', 'explore',
+    'styleguide', 'showcases', 'trending',
+    'stars', 'dashboard', 'notifications',
+    'search', 'developer', 'account',
+    'pulls', 'issues', 'features', 'contact',
+    'security', 'join', 'login', 'watching',
+    'new', 'integrations', 'gist', 'business',
+    'mirrors', 'open-source', 'personal',
+    'pricing'
+]
+const OSC_RESERVED_REPO_NAMES = ['followers', 'following', 'repositories']
+const OSC_404_SEL = '#parallax_wrapper'
+const OSC_PJAX_CONTAINER_SEL = '.git-project-content-wrapper, .context-loader-container, [data-pjax-container]'
+const OSC_CONTAINERS = '#git-header-nav'
+const OSC_RAW_CONTENT = 'body > pre'
+
+class Oschina extends PjaxAdapter {
+
+    constructor() {
+        super(['jquery.pjax.js'])
+    }
+
+    // @override
+    init($sidebar) {
+        const pjaxContainer = $(OSC_PJAX_CONTAINER_SEL)[0]
+        super.init($sidebar, { 'pjaxContainer': pjaxContainer })
+
+        // Fix #151 by detecting when page layout is updated.
+        // In this case, split-diff page has a wider layout, so need to recompute margin.
+        // Note that couldn't do this in response to URL change, since new DOM via pjax might not be ready.
+        const diffModeObserver = new window.MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+                if (~mutation.oldValue.indexOf('split-diff') ||
+                        ~mutation.target.className.indexOf('split-diff')) {
+                    return $(document).trigger(EVENT.LAYOUT_CHANGE)
+                }
+            })
+        })
+
+        diffModeObserver.observe(document.body, {
+            attributes: true,
+            attributeFilter: ['class'],
+            attributeOldValue: true
+        })
+    }
+
+    // @override
+    getCssClass() {
+        return 'octotree_oschina_sidebar'
+    }
+
+    // @override
+    canLoadEntireTree() {
+        return true
+    }
+
+    // @override
+    getCreateTokenUrl() {
+        return `${location.protocol}//${location.host}/settings/tokens/new`
+    }
+
+    // @override
+    updateLayout(togglerVisible, sidebarVisible, sidebarWidth) {
+        const SPACING = 232
+        const $containers = $(OSC_CONTAINERS)
+        const autoMarginLeft = ($(document).width() - $containers.width()) / 2
+        const WIDTH = $(document).width() - SPACING
+        const shouldPushLeft = sidebarVisible && (autoMarginLeft <= sidebarWidth + SPACING)
+
+        $('html').css('margin-left', shouldPushLeft ? sidebarWidth : '')
+        $containers.css('margin-left', shouldPushLeft ? SPACING : '')
+        $containers.css('width', shouldPushLeft ? WIDTH : '')
+    }
+
+    // @override
+    getRepoFromPath(showInNonCodePage, currentRepo, token, cb) {
+
+        // 404 page, skip
+        if ($(OSC_404_SEL).length) {
+            return cb()
+        }
+
+        // Skip raw page
+        if ($(OSC_RAW_CONTENT).length) {
+            return cb()
+        }
+
+        // (username)/(reponame)[/(type)]
+        const match = window.location.pathname.match(/([^\/]+)\/([^\/]+)(?:\/([^\/]+))?/)
+        if (!match) {
+            return cb()
+        }
+
+        const username = match[1]
+        const reponame = match[2]
+
+        // Not a repository, skip
+        if (~OSC_RESERVED_USER_NAMES.indexOf(username) ||
+                ~OSC_RESERVED_REPO_NAMES.indexOf(reponame)) {
+            return cb()
+        }
+
+        // Skip non-code page unless showInNonCodePage is true
+        if (!showInNonCodePage && match[3] && !~['tree', 'blob'].indexOf(match[3])) {
+            return cb()
+        }
+
+        // Get branch by inspecting page, quite fragile so provide multiple fallbacks
+        const branch =
+        // Code page
+        $('.branch-select-menu .select-menu-item.selected').data('name') ||
+        // Pull requests page
+        ($('.commit-ref.base-ref').attr('title') || ':').match(/:(.*)/)[1] ||
+        // Reuse last selected branch if exist
+        (currentRepo.username === username && currentRepo.reponame === reponame && currentRepo.branch) ||
+        // Get default branch from cache
+        this._defaultBranch[username + '/' + reponame]
+
+        // Still no luck, get default branch for real
+        const repo = { username: username, reponame: reponame, branch: branch }
+
+        if (repo.branch) {
+            cb(null, repo)
+        } else {
+            this._get(null, { repo, token }, (err, data) => {
+                if (err) return cb(err)
+                repo.branch = this._defaultBranch[username + '/' + reponame] = data.default_branch || 'master'
+                cb(null, repo)
+            })
+        }
+    }
+
+    // @override
+    selectFile(path) {
+        const $pjaxContainer = $(OSC_PJAX_CONTAINER_SEL)
+        super.selectFile(path, { '$pjaxContainer': $pjaxContainer })
+    }
+
+    // @override
+    loadCodeTree(opts, cb) {
+        opts.encodedBranch = encodeURIComponent(decodeURIComponent(opts.repo.branch))
+        opts.path = (opts.node && (opts.node.sha || opts.encodedBranch)) ||
+            (opts.encodedBranch + '?recursive=1')
+        this._loadCodeTree(opts, null, cb)
+    }
+
+    // @override
+    _getTree(path, opts, cb) {
+        this._get(`/git/trees/${path}`, opts, (err, res) => {
+            if (err) cb(err)
+            else cb(null, res.tree)
+        })
+    }
+
+    // @override
+    _getSubmodules(tree, opts, cb) {
+        const item = tree.filter((item) => /^\.gitmodules$/i.test(item.path))[0]
+        if (!item) return cb()
+
+        this._get(`/git/blobs/${item.sha}`, opts, (err, res) => {
+            if (err) return cb(err)
+            const data = atob(res.content.replace(/\n/g, ''))
+            cb(null, parseGitmodules(data))
+        })
+    }
+
+    _get(path, opts, cb) {
+        const host = 'http://git.oschina.net/api/v5'
+        var url = `${host}/repos/${opts.repo.username}/${opts.repo.reponame}${path || ''}`
+        const cfg = { url, method: 'GET', cache: false }
+
+        if (opts.token) {
+            url += (url.endsWith("?") ? "&" : "?") + `access_token=${opts.token}`
+        }
+
+        $.ajax(cfg)
+            .done((data) => {
+                if (path && path.indexOf('/git/trees') === 0 && data.truncated) {
+                    this._handleError({ status: 206 }, cb)
+                }
+                else cb(null, data)
+            })
+            .fail((jqXHR) => this._handleError(jqXHR, cb))
+    }
+}
